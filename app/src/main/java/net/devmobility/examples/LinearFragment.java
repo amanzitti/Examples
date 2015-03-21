2c098d8085a7bac77a05ad4faae67e90efa49af2
Ann Marie Mossman <geekopolis@hotmail.com>
2015-03-21 15:47:32 -0700

    Initial commit

---
+++ app/src/main/java/net/devmobility/examples/LinearFragment.java
@@ -0,0 +1,23 @@
+package net.devmobility.examples;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ammo on 3/19/2015.
+ */
+public class LinearFragment extends Fragment {
+
+        public LinearFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_linear_default,  container, false);
+            return rootView;
+        }
+}
